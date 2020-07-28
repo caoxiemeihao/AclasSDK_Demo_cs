626410a569d9dbc27170a28ff550738543cff1f5
草鞋没号 <308487730@qq.com>
2020-07-28 16:49:50 +0800

    新增单文件版

---
+++ AclasFor_node_SingleFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AclasFor_node_SingleFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+        }
+    }
+}
